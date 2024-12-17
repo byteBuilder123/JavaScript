5972e10feee1ad7f260a5654e209fa2ddeda7e4c
Syeda Saba Fatima <syedasabafatima7222@gmail.com>
2024-12-17 06:58:31 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 9) using nodejs not browser
+
+console.log(6  
+    + 9); // code readability should be high
+console.log("Saba")
+
+
+let name = "saba"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof Number); 
+console.log(typeof String); 
